f581560f8299244a3c318fdd2cd91907440d23a6
Nicolas <rey.nicolas.92@gmail.com>
2023-01-07 14:26:41 -0300

    cuarto commit
    ajustes visuales, validacion

---
+++ Desafio-Final-Curso-CSharp-VendedorDeTienda/Desafio-Final-Curso-CSharp-VendedorDeTienda/Models/Tienda.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Final_Curso_CSharp_VendedorDeTienda.Models
+{
+    public interface ITienda
+    {
+        
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public Dictionary<string, IPrenda> IdsToPrendas { get; set; }
+
+        public void AgregarPrenda(IPrenda prenda);
+
+        public void AgregarListaDePrendas(List<IPrenda> prendas);
+    }
+    public class Tienda: ITienda
+    {
+        string nombre, direccion;
+        Dictionary<string, IPrenda> idsToPrendas;
+
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Direccion { get { return direccion; } set { direccion = value; } }
+        public Dictionary<string, IPrenda> IdsToPrendas { get { return idsToPrendas; } set { idsToPrendas = value; } }
+
+        public void AgregarPrenda(IPrenda prenda)
+        {
+            idsToPrendas.Add(prenda.Id, prenda);
+        }
+
+        public void AgregarListaDePrendas(List<IPrenda> prendas)
+        {
+            foreach (IPrenda prenda in prendas)
+            {
+                AgregarPrenda(prenda);
+            }
+        }
+
+
+    }
+
+    
+}
